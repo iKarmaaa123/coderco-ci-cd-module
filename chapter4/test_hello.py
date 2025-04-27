1b563687630babcb7dc6571e0592a4fae82aca2a
iKarmaaa123 <waael@hotmail.co.uk>
2025-04-27 20:05:16 +0100

    combined python builds with matrix builds

--- chapter4/test_hello.py
+++ chapter4/test_hello.py
@@ -2,5 +2,8 @@ import unittest
 from hello import say_hello
 
 class TestHello(unittest.TestCase):
-    def say_hello(self):
-        self.assertEqual(say_hello(), "Hello, World!")
+    def test_say_hello(self):
+        self.assertEqual(test_say_hello(), "Hello, World!")
+
+if __name__ == "__main__":
+    unittest.main()
